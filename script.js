0139d301594e5c52de4312001dc6c482589ce68b
H_Innos <henrik.innos@gmail.com>
2022-12-04 19:41:23 +0200

    JAVA SCRIPT

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const btn = document.getElementsByClassName('buy-1');
+// Henrik Innos 2022
+function initialize()
+{
+    let btns = document.querySelectorAll('button');
 
-btn.addEventListener('click', function handleClick() {
-    btn.textContent = 'Lisatud ostukorvi'
-});
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Poodi";
+    btns.forEach(function (i) {
+        i.addEventListener('click', function() {
+            i.textContent = 'Lisatud ostukorvi'
+            setTimeout(function() {
+                i.textContent = 'Osta'
+            }, 2500)
+        });
+    });
 }
+
+document.addEventListener("DOMContentLoaded",initialize,false);
+
+
