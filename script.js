c4540c94f887377b3dd57c2773e1c11b0ff8a5e4
EliisabetK <117184918+EliisabetK@users.noreply.github.com>
2022-12-05 17:22:23 +0200

    lisasin ühe lause :)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     https://www.codeinwp.com/snippets/add-event-listener-to-multiple-elements-with-javascript/
     Henrik Innos 2022
 */
-
+/*javascript muudab kataloogi lehel "osta" nupu teksti vajutusel tekstiks "lisatud ostukorvi"*/
 function initialize()
 {
     let btns = document.querySelectorAll('button'); //valib iga button elemendi lehel
